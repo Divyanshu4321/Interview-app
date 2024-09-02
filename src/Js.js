54bc1885429c3ebe19bef19e8a902585176439df
Divyanshu4321 <divyanshuparmar00@gmail.com>
2024-09-02 07:00:16 +0530

    update

--- src/Js.js
+++ src/Js.js
@@ -23,10 +23,28 @@ const Js = () => {
               what is the datatypes ?
               <br />
               <h4 className="mt-3">
-                a variable is a named storage location that can hold a value.
-                Variables are used to store data values that can be accessed and
-                manipulated throughout a program. Here are some key points about
-                variables:
+                In JavaScript, data types are the different kinds of values that
+                variables can hold. Here are the two main data types: in present
+                in js
+                <ol>
+                  <li>
+                    Primitive data-type {" > "}
+                    <ol>
+                      <li>Number</li>
+                      <li>string</li>
+                      <li>boolean</li>
+                      <li>null</li>
+                      <li>undefined</li>
+                    </ol>
+                  </li>
+                  <li>
+                    Non Primitive data-type
+                    <ol>
+                      <li>array</li>
+                      <li>object</li>
+                    </ol>
+                  </li>
+                </ol>
               </h4>
             </li>
             <li className="my-5">
@@ -57,6 +75,49 @@ const Js = () => {
                 declarations are moved to the top of their containing scope
                 during the compilation phase. This means you can use variables
                 and functions before you declare them in your code.
+              </h4>
+            </li>
+            <li className="my-5">
+              what is the Array ? Give me some example of Array methods ?
+              <br />
+              <h4 className="mt-3">
+                An array is a sequential colletion of a data type use to store
+                multiple data in a single variable there are containe many
+                method like
+                <ol>
+                  <li>concat(): these method are use to merge two arrays</li>
+                  <li>
+                    push(): these method are use to add one or more element in
+                    array
+                  </li>
+                  <li>
+                    pop(): pop method are use to remove last element of array
+                  </li>
+                  <li>
+                    shift(): shift method are use to remove first element from
+                    array
+                  </li>
+                </ol>
+              </h4>
+            </li>
+            <li className="my-5">
+              what is the object and tell some importance method of object ?
+              <br />
+              <h4 className="mt-3">
+                an object is a complex data type that allows you to store and
+                organize data in a structured way. An object can be thought of
+                as a collection of key-value pairs, where each key is a unique
+                identifier and each value can be any valid JavaScript data type,
+                including other objects <br />
+                  Here are some important methods of objects in JavaScript:
+                <ol>
+                  <li>
+                      Object.keys(): - Returns an array of all the object's own property names.
+                  </li>
+                  <li>
+                      Object.values(): - Returns an array of all the object's own property values.
+                  </li>
+                </ol>
               </h4>
             </li>
           </h2>
